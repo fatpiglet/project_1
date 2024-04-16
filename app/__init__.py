a924f31614aa4b9899e4a92cc9de797b6394c854
fatpiglet <143342396+fatpiglet@users.noreply.github.com>
2024-04-17 01:16:28 +0530

    DAY 8

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -7,6 +7,14 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
+from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
+
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,7 +22,11 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page')
 mail = Mail(app)
+moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale)
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -40,5 +52,6 @@ if not app.debug:
 
             app.logger.setLevel(logging.INFO)
             app.logger.info('Project_1 startup')
+
 
 from app import routes, models, errors
